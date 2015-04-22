04f2ad681889bb1f3cc21d5e9b55655dbf531c39
BOUKOUFALLAH Brahim <boukoufallah.brahim@gmail.com>
2015-04-22 14:49:53 +0200

    Compile the foundation scss files with gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,33 +1,35 @@
 'use strict';
 
-var src          = [],
-    web          = []
+var src             = [],
+    web             = [],
+    bower           = []
 ;
 
-src['style']     = "src/Ressources/styles/**/*.scss";
-src['template']  = "templates/**/*";
-src['script']    = "src/Ressources/js/**/*.js";
-web['style']     = "web/css/";
-web['script']    = "web/js/";
+src['style']        = "src/Ressources/styles/**/*.scss";
+src['template']     = "templates/**/*";
+src['script']       = "src/Ressources/js/**/*.js";
+web['style']        = "web/css/";
+web['script']       = "web/js/";
+bower['foundation'] = "bower_components/foundation/scss/*.scss";
 
-var autoprefixer = require('gulp-autoprefixer'),
-    chmod        = require('gulp-chmod'),
-    concat       = require('gulp-concat'),
-    del          = require('del'),
-    gulp         = require('gulp'),
-    jshint       = require('gulp-jshint'),
-    livereload   = require('gulp-livereload'),
-    minifycss    = require('gulp-minify-css'),
-    notify       = require('gulp-notify'),
-    rename       = require('gulp-rename'),
-    sass         = require('gulp-sass'),
-    uglify       = require('gulp-uglify')
+var autoprefixer    = require('gulp-autoprefixer'),
+    chmod           = require('gulp-chmod'),
+    concat          = require('gulp-concat'),
+    del             = require('del'),
+    gulp            = require('gulp'),
+    jshint          = require('gulp-jshint'),
+    livereload      = require('gulp-livereload'),
+    minifycss       = require('gulp-minify-css'),
+    notify          = require('gulp-notify'),
+    rename          = require('gulp-rename'),
+    sass            = require('gulp-sass'),
+    uglify          = require('gulp-uglify')
 ;
 
 // Task to compile Sass files
 gulp.task('styles', function() {
-    gulp.src(src['style'])
-        .pipe(sass({ errLogToConsole: true }))
+    gulp.src([ src['style'], bower['foundation']])
+        .pipe(sass({errLogToConsole: true}))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
         .pipe(minifycss({keepSpecialComments: 0}))
         .pipe(concat({ path: 'app.min.css'}))
