b348784d9b885b19eaca7ff90452378088ec8e69
Baptiste Bouchereau <baptiste.bouchereau@gmail.com>
2015-06-09 11:04:18 +0200

    Respect php coding standards

--- src/console.php
+++ src/console.php
@@ -1,18 +1,24 @@
 <?php
 
+/**
+ * Console to register commands
+ */
+
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
 
 $console = new Application('My Silex Application', 'n/a');
-$console->getDefinition()->addOption(new InputOption(
-    '--env',
-    '-e',
-    InputOption::VALUE_REQUIRED,
-    'The Environment name.',
-    'dev'
-));
+$console->getDefinition()->addOption(
+    new InputOption(
+        '--env',
+        '-e',
+        InputOption::VALUE_REQUIRED,
+        'The Environment name.',
+        'dev'
+    )
+);
 $console->setDispatcher($app['dispatcher']);
 $console
     ->register('my-command')
