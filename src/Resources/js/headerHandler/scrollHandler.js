abac94f85dd2be75189461da86acadcfb5141b23
BOUKOUFALLAH Brahim <boukoufallah.brahim@gmail.com>
2015-06-19 16:29:45 +0200

    Remove useless function

--- src/Resources/js/headerHandler/scrollHandler.js
+++ src/Resources/js/headerHandler/scrollHandler.js
@@ -27,7 +27,6 @@ if (window.matchMedia('(min-width: 40.063em)').matches) {
             $('section.left-small').css('border', '.2em solid #FFF');
             $('section.middle.idci-logo').addClass('show-for-small');
             $('section.right-small').css('border', '.2em solid #FFF');
-            cancelSizeLogo($('h1 .idci-logo'));
 
             $('a.idci-menu').css('color', '#FFF');
             $('a.idci-search').css('color', '#FFF');
@@ -37,18 +36,10 @@ if (window.matchMedia('(min-width: 40.063em)').matches) {
     var resizeLogo = function(element) {
         element
             .css('background-size', '400px')
-            .css('background-position', '0 -115px')
+            .css('background-position', '0 -114px')
             .css('height', '44px')
             .css('margin-left', '2em')
             .css('width', '100%')
-        ;
-    };
-
-    var cancelSizeLogo = function(element) {
-        element
-            .css('background-position', '0 0')
-            .css('height', '73px')
-            .css('width', '260px')
         ;
     };
 }
