bdc14e69e6faf9d6d1548832b1ed7d043c3281ed
BOUKOUFALLAH Brahim <boukoufallah.brahim@gmail.com>
2015-06-30 17:06:38 +0200

    Create a markdown service provider

---
+++ src/Providers/MarkdownParserServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Providers;
+
+use Silex\Application;
+use Pimple\ServiceProviderInterface;
+use Pimple\Container;
+use Knp\Bundle\MarkdownBundle\Parser\MarkdownParser;
+use SilexMarkdown\MarkdownExtension\MarkdownTwigExtension;
+
+class MarkdownParserServiceProvider implements ServiceProviderInterface
+{
+	public function boot(Application $app)
+    {
+    }
+
+    public function register(Container $app)
+    {
+        $app['markdown'] = function () use ($app) {
+            $features = isset($app['markdown.features']) ? $app['markdown.features'] : array();
+
+            return new MarkdownParser($features);
+        };
+
+        if (isset($app['twig'])) {
+            $app['twig'] = $app->extend('twig', function ($twig, $app) {
+                $twig->addExtension(new MarkdownTwigExtension($app['markdown']));
+
+                return $twig;
+            });
+        }
+    }
+}
