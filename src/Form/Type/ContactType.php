f947699d0cffcf66b9ba773257dbba2fa31b6594
BOUKOUFALLAH Brahim <boukoufallah.brahim@gmail.com>
2015-07-09 11:02:44 +0200

    Add constraints for contact form & translate labels

--- src/Form/Type/ContactType.php
+++ src/Form/Type/ContactType.php
@@ -10,56 +10,55 @@ namespace Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints as Constraints;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $notBlankConstraint = new Constraints\NotBlank(array(
+            'message' => 'Please complete this field.'
+        ));
+
         $builder
-            ->add(
-                'company',
-                'text',
-                array(
-                    'required' => false,
+            ->add('company', 'text', array(
+                'required' => false,
+            ))
+            ->add('website', 'url', array(
+                'required' => false,
+                'constraints' => array(
+                    new Constraints\Url()
                 )
-            )
-            ->add(
-                'website',
-                'url',
-                array(
-                    'required' => false,
+            ))
+            ->add('name', 'text', array(
+                'constraints' => array(
+                    $notBlankConstraint,
                 )
-            )
-            ->add(
-                'name',
-                'text',
-                array()
-            )
-            ->add(
-                'firstname',
-                'text',
-                array()
-            )
-            ->add(
-                'email',
-                'email',
-                array(
-                    'constraints' => array(
-                        new Assert\NotBlank()
-                    )
+            ))
+            ->add('firstname', 'text', array(
+                'constraints' => array(
+                    $notBlankConstraint,
                 )
-            )
-            ->add(
-                'phonenumber',
-                'number',
-                array()
-            )
-            ->add(
-                'project',
-                'textarea',
-                array()
-            );
+            ))
+            ->add('email', 'email', array(
+                'constraints' => array(
+                    $notBlankConstraint,
+                    new Constraints\Email(array(
+                        'message' => 'The email is not valid.'
+                    ))
+                )
+            ))
+            ->add('phonenumber', 'number', array(
+                'constraints' => array(
+                    $notBlankConstraint,
+                )
+            ))
+            ->add('project', 'textarea', array(
+                'constraints' => array(
+                    $notBlankConstraint,
+                )
+            ))
+        ;
     }
 
     /**
