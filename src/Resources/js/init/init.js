50d18694a6fa2be7cfeebd35537d1df39f54e21a
BOUKOUFALLAH Brahim <boukoufallah.brahim@gmail.com>
2015-07-29 14:09:39 +0200

    Add a active class in the aside menu element on scroll or click

--- src/Resources/js/init/init.js
+++ src/Resources/js/init/init.js
@@ -6,6 +6,8 @@ Modernizr.load({
     nope: ['polyfills/rem.js']
 });
 
+
+// Function to decode string into utf8
 String.prototype.decode = function(encoding) {
     var result = "";
 
@@ -32,3 +34,42 @@ String.prototype.decode = function(encoding) {
 
     return result;
 };
+
+// Function to highlight aside menu element referenced with id and href
+(function highlightNav() {
+    var prev; //keep track of previous selected link
+    var isVisible= function(el){
+        el = $(el);
+
+        if(!el || el.length === 0){
+            return false
+        }
+
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = el.offset().top;
+        var elemBottom = elemTop + el.height();
+        return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom));
+    };
+
+    $(window).scroll(function(){
+        $('.aside-navigation-menu a').each(function(index, el){
+            el = $(el);
+            if(isVisible(el.attr('href'))){
+                if(prev){
+                    prev.removeClass('active');
+                }
+                el.addClass('active');
+                prev = el;
+
+                //break early to keep highlight on the first/highest visible element
+                //remove this you want the link for the lowest/last visible element to be set instead
+                return false;
+            }
+        });
+    });
+
+    //trigger the scroll handler to highlight on page load
+    $(window).scroll();
+})();
