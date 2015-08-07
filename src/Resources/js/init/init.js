0851852e4af291719727d480652c58f8923d8f2f
BOUKOUFALLAH Brahim <boukoufallah.brahim@gmail.com>
2015-08-07 17:04:01 +0200

    Remove the fade effect on scroll

--- src/Resources/js/init/init.js
+++ src/Resources/js/init/init.js
@@ -68,21 +68,6 @@ String.prototype.decode = function(encoding) {
                 return false;
             }
         });
-
-        $('.fade-in-block').each( function(i){
-
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(document).scrollTop() + $('body, html').height();
-
-            /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
-            bottom_of_window = bottom_of_window + 200;
-
-            if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},500);
-
-            }
-        });
     });
 
     //trigger the scroll handler to highlight on page load
