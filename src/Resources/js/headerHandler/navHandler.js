b28a53a3b97071ed30f5e444f645ec457e9de042
Brahim BOUKOUFALLAH <brahim.boukoufallah@idci-consulting.fr>
2015-11-23 10:00:33 +0100

    Change the header design for mobile device according to Flavie's mockups

--- src/Resources/js/headerHandler/navHandler.js
+++ src/Resources/js/headerHandler/navHandler.js
@@ -1,15 +1,11 @@
 $('.menu-icon').on('click', function(event) {
     event.preventDefault();
 
-    var $nav     = $('nav.nav-menu');
-    var $navIcon = $('.menu-icon') ;
+    var $nav = $('nav.nav-menu'),
+        $hamburger = $('.hamburger')
+    ;
 
     $nav.toggleClass('open');
+    $hamburger.toggleClass('open');
     $nav.toggle('fast');
-
-    if ($nav.hasClass("open")) {
-        $navIcon.html("<i class='fa fa-times'></i>");
-    } else {
-        $navIcon.html("<i class='fa fa-bars'></i>");
-    }
 });
