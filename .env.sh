423fc4e9415186233182989f3bdef5a50ca8b19a
Lucas AUDART <lucas.audart@idci-consulting.fr>
2019-06-11 17:14:11 +0200

    Adding all translation in en and fr + few style
    - all the text in html.twig file follow the type.page.id format
    - first style for the references page, the service offers page and the courses page

--- .env.sh
+++ .env.sh
@@ -5,7 +5,6 @@ export CONTAINER_RELEASE_IMAGE=docker-registry.idci-consulting.fr/idci/website/p
 
 export APP_ENV="dev"
 export APP_DEBUG=1
-export APP_VERSION=3.0
 export APP_HOST="idci.docker"
 export DEV_USER_ID=1000
 export DEV_GROUP_ID=1000
--- src/Manager/BlogManager.php
+++ src/Manager/BlogManager.php
@@ -41,6 +41,7 @@ class BlogManager
             $article['summary'] = $this->parser->transformMarkdown(sprintf('%s ...', $matches[0]));
             $lastArticles[$key] = $article;
         }
+
         return $lastArticles;
     }
 
