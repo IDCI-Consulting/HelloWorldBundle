fd9921367ae1adaf4f4bc6736bb89afe24bf9405
Joris <joris.viniere@idci-consulting.fr>
2022-02-02 13:51:10 +0100

    WIP: add views and controllers

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/contact", name="contact_")
+ */
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET", "POST"}, name="contact")
+     */
+    public function contact(Request $request): Response
+    {
+        return $this->render('contact/index.html.twig');
+    }
+}
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/homepage", methods={"GET"}, name="homepage")
+     */
+    public function homepage(Request $request): Response
+    {
+        return $this->render('home/homepage.html.twig');
+    }
+}
