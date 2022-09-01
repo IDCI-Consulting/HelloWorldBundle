8d302c7d217779f33bfc7d3a90f72939c216e3ce
joris <joris.viniere@idci-consulting.fr>
2022-09-01 15:22:24 +0200

    add slick js part into document ready

--- assets/slick.js
+++ assets/slick.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
         element.classList.remove("keyframes");
         element.classList.add("carousel-single-item");
     });
-});
 
-$('.pic-ctn').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3500,
+    $('.pic-ctn').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3500,
+    });
 });
