c8911a2e449b5b5dbbfeccbf2ef5ce259dbd9bc3
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2023-10-16 22:11:30 +0200

    Add symfony profiler

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
