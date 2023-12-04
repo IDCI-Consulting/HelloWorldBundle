f153520ae01d58beaaef917c0d6d1d5819716ec1
Maëva GRONDIN <maeva.grondin@idci-consulting.fr>
2023-12-04 12:37:29 +0100

    Rework blog carousel

--- assets/slick.js
+++ assets/slick.js
@@ -6,6 +6,10 @@ $(document).ready(function() {
     });
 
     $('.pic-ctn').slick({
+        dots: false,
+        arrows: true,
+        prevArrow: '<button class="slide-arrow prev"><img src="/build/images/base/balise-left.svg" alt="previous arrow" class="slide-arrow-media"></button>',
+        nextArrow: '<button class="slide-arrow next"><img src="/build/images/base/balise-right.svg" alt="next arrow" class="slide-arrow-media"></button>',
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
