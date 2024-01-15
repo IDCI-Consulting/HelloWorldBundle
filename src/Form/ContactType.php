40ee17a74c04bac5461d3021d14aa913d6d8754e
Maëva GRONDIN <maeva.grondin@idci-consulting.fr>
2024-01-15 10:58:22 +0100

    Add missing function return type and config option

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -15,7 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class ContactType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('company', TextType::class, [
